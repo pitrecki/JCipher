71df27e0b7072de0b12c0cfbfc3a14659f74d4e4
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2016-12-12 13:13:38 +0100

    string KEYWORD instead of final String KEYWORD, changed tabula... name method to vinegereTableGenerate, added the logic of extending  length of the keyword (letter repeater) to the length text to encode/decode.

--- src/org/cipher/ciphtypes/polygraphic/VinegereCipher.java
+++ src/org/cipher/ciphtypes/polygraphic/VinegereCipher.java
@@ -3,20 +3,24 @@ package org.cipher.ciphtypes.polygraphic;
 import org.cipher.ciphtypes.Cipher;
 
 /**
+ *
+ * @author Piotr 'pitrecki' Nowak
+ * @version 0.0.1
  * Created by Pitrecki on 2016-12-11.
  */
 public class VinegereCipher extends Cipher
 {
-    private final String KEY_WORD;
-    //aka tabbula recta
+    //aka tabula recta
     private final char[][] VINEGERE_TABLE;
 
-    public VinegereCipher(String KEY_WORD) {
-        this.KEY_WORD = KEY_WORD;
-        this.VINEGERE_TABLE = tabulaRectaTableGenerate();
+    private  String keyword;
+
+    public VinegereCipher(String keyword) {
+        this.keyword = keyword.toUpperCase();
+        this.VINEGERE_TABLE = vinegereTableGenerate();
     }
 
-    private char[][] tabulaRectaTableGenerate() {
+    private char[][] vinegereTableGenerate() {
         //size 26x26
         char[][] vinegereTable = new char[ASCII_TABLE.length][ASCII_TABLE.length];
         int currentColumnIndex = 1;
@@ -35,18 +39,38 @@ public class VinegereCipher extends Cipher
         return vinegereTable;
     }
 
+    private void keyWordShifter(int cryptTextLength) {
+        if (cryptTextLength > keyword.length()) {
+            int index = 0;
+            char[] keywordCharArray = this.keyword.toCharArray();
+            StringBuilder builder = new StringBuilder(this.keyword);
+            for (int i = 0; i < (cryptTextLength - keyword.length()); i++) {
+                if (index == keyword.length())
+                    index = 0;
+                builder.append(keywordCharArray[index]);
+                index++;
+            }
+            this.keyword = builder.toString();
+        }
+        else if (cryptTextLength < keyword.length()) {
+            this.keyword = keyword.trim().substring(0, cryptTextLength);
+        }
+    }
+
     @Override
     public void encrypt(String inputText) {
-
+        if (inputText.length() != keyword.length())
+            keyWordShifter(inputText.replaceAll("[ ]", "").length());
     }
 
     @Override
     public void decrypt(String inputText) {
-
+        if (inputText.length() != keyword.length())
+            keyWordShifter(inputText.replaceAll("[ ]", "").length());
     }
 
     @Override
     public String prepareDataToPrint() {
-        return null;
+        return getProcessedText();
     }
 }
