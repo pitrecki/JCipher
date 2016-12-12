1dc7b918dd4cea7f30da1690e8f65c29e196e298
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2016-12-12 16:38:00 +0100

    Full implementation of Vinegere cipher.

--- src/org/cipher/ciphtypes/polygraphic/VinegereCipher.java
+++ src/org/cipher/ciphtypes/polygraphic/VinegereCipher.java
@@ -1,8 +1,19 @@
 package org.cipher.ciphtypes.polygraphic;
 
 import org.cipher.ciphtypes.Cipher;
+import org.cipher.utils.Variant;
 
 /**
+ * The Vigenère cipher is a method of encrypting alphabetic text by using a series of different Caesar
+ * ciphers based on the letters of a keyword. It is a simple form of polyalphabetic substitution.The
+ * Vigenère (French pronunciation: ​[viʒnɛːʁ]) cipher has been reinvented many times. The method was
+ * originally described by Giovan Battista Bellaso in his 1553 book La cifra del. Sig. Giovan Battista
+ * Bellaso; however, the scheme was later misattributed to Blaise de Vigenère in the 19th century, and
+ * is now widely known as the "Vigenère cipher". Though the cipher is easy to understand and implement,
+ * for three centuries it resisted all attempts to break it; this earned it the description le chiffre
+ * indéchiffrable (French for 'the indecipherable cipher'). Many people have tried to implement encryption
+ * schemes that are essentially Vigenère ciphers.[3] Friedrich Kasiski was the first to publish a general
+ * method of deciphering a Vigenère cipher, in 1863.
  *
  * @author Piotr 'pitrecki' Nowak
  * @version 0.0.1
@@ -19,6 +30,16 @@ public class VinegereCipher extends Cipher
         this.keyword = keyword.toUpperCase();
         this.VINEGERE_TABLE = vinegereTableGenerate();
     }
+
+    /**
+     * To encrypt, a table of alphabets can be used, termed a tabula recta, Vigenère square, or Vigenère
+     * table. It consists of the alphabet written out 26 times in different rows, each alphabet shifted
+     * cyclically to the left compared to the previous alphabet, corresponding to the 26 possible Caesar
+     * ciphers. At different points in the encryption process, the cipher uses a different alphabet from
+     * one of the rows. The alphabet used at each point depends on a repeating keyword.
+     *
+     * @return generated Vinegere matrix
+     */
 
     private char[][] vinegereTableGenerate() {
         //size 26x26
@@ -39,7 +60,14 @@ public class VinegereCipher extends Cipher
         return vinegereTable;
     }
 
-    private void keyWordShifter(int cryptTextLength) {
+    /**
+     * The person sending the message chooses a keyword and repeats it until it matches the length of the
+     * plaintext
+     *
+     * @param cryptTextLength length of plaintext
+     */
+
+    private void keywordShifter(int cryptTextLength) {
         if (cryptTextLength > keyword.length()) {
             int index = 0;
             char[] keywordCharArray = this.keyword.toCharArray();
@@ -59,14 +87,44 @@ public class VinegereCipher extends Cipher
 
     @Override
     public void encrypt(String inputText) {
-        if (inputText.length() != keyword.length())
-            keyWordShifter(inputText.replaceAll("[ ]", "").length());
+        cipherProccessing(inputText, Variant.ENCRYPT);
     }
 
     @Override
     public void decrypt(String inputText) {
-        if (inputText.length() != keyword.length())
-            keyWordShifter(inputText.replaceAll("[ ]", "").length());
+        cipherProccessing(inputText, Variant.DECRYPT);
+    }
+
+    /**
+     *
+     * @param text to decode or encode
+     * @param eVariant enuma type eq. DECRYPT or ENCRYPT
+     */
+
+    private void cipherProccessing(String text, Variant eVariant) {
+        text = text.replaceAll("[ ]", "");
+        if (text.length() != keyword.length())
+            keywordShifter(text.length());
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < keyword.length(); i++) {
+            int keywordCurrentLetter = this.keyword.charAt(i);
+            int inputTextCurrentLetter = text.charAt(i);
+            int result = 0;
+            switch (eVariant) {
+                case ENCRYPT:
+                    result = (keywordCurrentLetter + inputTextCurrentLetter) % ASCII_TABLE.length;
+                    break;
+                case DECRYPT:
+                    result = (inputTextCurrentLetter - keywordCurrentLetter) % ASCII_TABLE.length;
+                    if (result < 0)
+                        result = ASCII_TABLE.length - Math.abs(result);
+                    break;
+            }
+            builder.append(ASCII_TABLE[result]);
+        }
+
+        setProcessedText(builder.toString());
     }
 
     @Override
