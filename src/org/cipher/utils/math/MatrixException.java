198708e951aa251bb3efbbfdf0e67cfc0a3c2d48
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2016-12-23 15:15:59 +0100

    Renamed IvalidMatrixException to MatrixException, also code uccurency refactored.

--- src/org/cipher/utils/math/MatrixException.java
+++ src/org/cipher/utils/math/MatrixException.java
@@ -3,17 +3,17 @@ package org.cipher.utils.math;
 /**
  * Created by Pitrecki on 2016-11-11.
  */
-public class InvalidMatrixException extends Exception
+public class MatrixException extends Exception
 {
-    public InvalidMatrixException(String message) {
+    public MatrixException(String message) {
         super(message);
     }
 
-    public InvalidMatrixException(String message, Throwable cause) {
+    public MatrixException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public InvalidMatrixException(Throwable cause) {
+    public MatrixException(Throwable cause) {
         super(cause);
     }
 
