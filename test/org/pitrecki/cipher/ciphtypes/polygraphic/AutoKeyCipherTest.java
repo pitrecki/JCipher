87896913014241363d11d2df72e7e671d0f51374
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2016-12-28 21:01:25 +0100

    Created unit classes for future tests.

---
+++ test/org/pitrecki/cipher/ciphtypes/polygraphic/AutoKeyCipherTest.java
@@ -0,0 +1,13 @@
+package org.pitrecki.cipher.ciphtypes.polygraphic;
+
+import org.junit.jupiter.api.Disabled;
+
+/**
+ * @author Piotr 'pitrecki' Nowak
+ *         Created by Pitrecki on 2016-12-28.
+ */
+@Disabled
+class AutoKeyCipherTest
+{
+
+}
---
+++ test/org/pitrecki/cipher/ciphtypes/substition/complex/PolybiusSquareCipherTest.java
@@ -0,0 +1,13 @@
+package org.pitrecki.cipher.ciphtypes.substition.complex;
+
+import org.junit.jupiter.api.Disabled;
+
+/**
+ * @author Piotr 'pitrecki' Nowak
+ *         Created by Pitrecki on 2016-12-28.
+ */
+@Disabled
+class PolybiusSquareCipherTest
+{
+
+}
