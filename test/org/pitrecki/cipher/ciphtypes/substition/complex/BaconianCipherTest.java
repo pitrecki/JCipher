a4f7c0732a5d8ccf06de27639e3ea5752b37b460
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2016-12-30 17:41:45 +0100

    Unit tests for baconian cipher (not fully implemented yet).

--- test/org/pitrecki/cipher/ciphtypes/substition/complex/BaconianCipherTest.java
+++ test/org/pitrecki/cipher/ciphtypes/substition/complex/BaconianCipherTest.java
@@ -1,13 +1,51 @@
 package org.pitrecki.cipher.ciphtypes.substition.complex;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.pitrecki.cipher.ciphtypes.Cipher;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.pitrecki.cipher.ciphtypes.substition.complex.BaconianCipher.Variant.STANDARD;
+import static org.pitrecki.cipher.utils.TestContainer.*;
 
 /**
  * @author Piotr 'pitrecki' Nowak
  *         Created by Pitrecki on 2016-12-28.
  */
-@Disabled
 class BaconianCipherTest
 {
+    private Cipher baconianCipher;
 
+    List<String> listInit(String... params) {
+        return Arrays.stream(params).map(s ->
+                s.replaceAll("[\\W\\p{Digit}\\p{Punct}]", "").toUpperCase()).collect(Collectors.toList());
+    }
+
+    @Test
+    @DisplayName("Testing of text encryption using standard type")
+    void testStandardEncryption() {
+        this.baconianCipher = new BaconianCipher(STANDARD);
+
+        List<String> plaintextList = listInit(ALPHABET, SHORT_MESSAGE, LONG_MESSAGE);
+        List<String> expectedList = listInit(
+/* ALPHABET*/                "AAAAAAAAABAAABAAAABBAABAAAABABAABBAAABBBABAAAABAAAABAABABABAABABBABB" +
+                                      "AAABBABABBBAABBBBBAAAABAAABBAABABAABBBAABBBABAABABABBABBABABBB",
+/* SHORT_MESSAGE*/                    "BAAABBAABABAAAAABAAAABAABAABAAABBAAABBABBABAA",
+/* LONG_MESSAGE*/                     "BAABAAABBBAABAAAABBAABBABABABAAAABBABAAABAAABAAAABBAABBBAAAAABAAA" +
+                                      "AABAAAAABBABAAAABBAAABBABBAAAAABBABABBAAABBAB"
+        );
+
+        List<String> actuaList = Stream.of(plaintextList).flatMap(strings ->
+                strings.stream().map(s -> {
+                    baconianCipher.encrypt(s);
+                    return baconianCipher.getProcessedText();
+                } )).collect(Collectors.toList());
+
+        assertEquals(expectedList, actuaList);
+    }
 }
